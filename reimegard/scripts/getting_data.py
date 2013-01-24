25f3c03f480cae25718973aed0ecc58227ff856e
Johan Reimegård <johan.reimegard@scilifelab.se>
2013-01-24 15:01:11 +0100

    changed lastname to reimegard

--- reimegard/scripts/getting_data.py
+++ reimegard/scripts/getting_data.py
@@ -1,4 +1,4 @@
-from lastname import calculateFractionOfOutage
+from reimegard import calculateFractionOfOutage
 xml_file =  'http://www.grandcentral.org/developers/data/nyct/nyct_ene.xml'
 REASON= 'REPAIR'
 
